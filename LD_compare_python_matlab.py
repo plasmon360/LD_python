fac24b1eef0f1e9b2efd425258af693066f5f7eb
Bala Krishna Juluri <bkj@pienergy.com>
2015-09-09 11:53:06 -0700

    Added xlabels and ylabels

--- LD_compare_python_matlab.py
+++ LD_compare_python_matlab.py
@@ -12,13 +12,19 @@ print gold.n
 print gold.k
 
 data = np.loadtxt(os.path.join(os.path.dirname(__file__), r'LD_bora_ung_code_comparision\eps_Au_ld_bora.dat'))
-f,ax = plt.subplots(nrows = 1, ncols = 2)
+f,ax = plt.subplots(nrows = 1, ncols = 2, figsize = (15,6))
 
 ax[0].plot(1E9*lamda, gold.epsilon_real, 'bs', label = 'LD.py')
 ax[0].plot(1E9*data[:, 0], data[:, 1], '-r', label = 'LD.m')
+ax[0].set_xlabel('Wavelength (nm)')
+ax[0].set_ylabel('Real(epsilon)')
 
 ax[1].plot(1E9*lamda, gold.epsilon_imag, 'bs', label = 'LD.py')
 ax[1].plot(1E9*data[:, 0], data[:, 2], '-r', label = 'LD.m')
+ax[1].set_xlabel('Wavelength (nm)')
+ax[1].set_ylabel('Imag (epsilon)')
+f.suptitle('Epsilon of {0}: {1} model'.format(gold.material, gold.model))
+
 plt.legend()
-plt.show()
 plt.savefig('LD_python_matlab_comp.png')
+plt.show()
